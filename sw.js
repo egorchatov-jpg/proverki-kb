77891153fa523e7647b77d0ae21462b27fda7804
egorchatov-jpg <egorchatov@gmail.com>
2026-07-05 22:05:32 +0300

    Fix: центровка плашек (display flex), шрифт системный, счётчик проверок двустрочный

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pkb-v8';
+const CACHE = 'pkb-v9';
 const PRECACHE = ['/', '/manifest.json', '/apple-touch-icon.png', '/favicon.png', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
