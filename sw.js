7d7d2ba15a2fdd849272e3d651d910a3b6d6cb77
egorchatov-jpg <egorchatov@gmail.com>
2026-07-12 16:23:58 +0300

    fix: deadline label renamed to Vypolnit do, navy row opacity increased

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pkb-v58';
+const CACHE = 'pkb-v59';
 const PRECACHE = ['/', '/manifest.json', '/apple-touch-icon.png', '/favicon.png', '/icon-192.png', '/icon-512.png', '/badge.svg'];
 
 self.addEventListener('install', e => {
