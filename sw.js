1f66eb94b26f8f325d7244418b9e541e107cf992
egorchatov-jpg <egorchatov@gmail.com>
2026-07-12 22:22:42 +0300

    fix: unify filter drum capsule width across list overlays (pkb-v77)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pkb-v76';
+const CACHE = 'pkb-v77';
 const PRECACHE = ['/', '/manifest.json', '/apple-touch-icon.png', '/favicon.png', '/icon-192.png', '/icon-512.png', '/badge.svg'];
 
 self.addEventListener('install', e => {
