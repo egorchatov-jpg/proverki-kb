e194f44e2b082e109e9cfb7ed126e2bc3f545810
egorchatov-jpg <egorchatov@gmail.com>
2026-07-12 16:28:32 +0300

    fix: enable pull-to-refresh on corrective list screen

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pkb-v59';
+const CACHE = 'pkb-v60';
 const PRECACHE = ['/', '/manifest.json', '/apple-touch-icon.png', '/favicon.png', '/icon-192.png', '/icon-512.png', '/badge.svg'];
 
 self.addEventListener('install', e => {
