173f750c5fa7631d9b0b4d7f86a5b299bf235ee6
egorchatov-jpg <egorchatov@gmail.com>
2026-07-14 21:50:07 +0300

    fix: narrow passport column in barriers PK settings (pkb-v93)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pkb-v92';
+const CACHE = 'pkb-v93';
 const PRECACHE = ['/', '/manifest.json', '/apple-touch-icon.png', '/favicon.png', '/icon-192.png', '/icon-512.png', '/badge.svg'];
 
 self.addEventListener('install', e => {
