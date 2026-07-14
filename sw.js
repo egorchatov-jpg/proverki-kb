951150cb8329351b83121d81ca0e2ad99be3c99e
egorchatov-jpg <egorchatov@gmail.com>
2026-07-14 21:25:55 +0300

    feat: record edit, corrective list and detail fixes (pkb-v92)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pkb-v87';
+const CACHE = 'pkb-v92';
 const PRECACHE = ['/', '/manifest.json', '/apple-touch-icon.png', '/favicon.png', '/icon-192.png', '/icon-512.png', '/badge.svg'];
 
 self.addEventListener('install', e => {
