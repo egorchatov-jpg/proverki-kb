0695bd808a842f414e190ba6f9ecdcadb71c1584
egorchatov-jpg <egorchatov@gmail.com>
2026-07-15 21:04:27 +0300

    feat: счётчики чек-боксов в настройках + копирование состояния барьеров при новом годе (pkb-v118)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pkb-v117';
+const CACHE = 'pkb-v118';
 const PRECACHE = ['/', '/manifest.json', '/apple-touch-icon.png', '/favicon.png', '/icon-192.png', '/icon-512.png', '/badge.svg'];
 
 self.addEventListener('install', e => {
