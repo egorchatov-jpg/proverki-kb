f5fef39972da941b857d78d4df2459c29997f310
egorchatov-jpg <egorchatov@gmail.com>
2026-07-16 14:49:12 +0300

    Barriers PK: choice overlay for add passport or barrier (pkb-v138)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pkb-v137';
+const CACHE = 'pkb-v138';
 const PRECACHE = ['/', '/manifest.json', '/apple-touch-icon.png', '/favicon.png', '/icon-192.png', '/icon-512.png', '/badge.svg'];
 
 self.addEventListener('install', e => {
