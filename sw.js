3ae52309439a7078b9a869bccabe912ec57d4d9e
egorchatov-jpg <egorchatov@gmail.com>
2026-07-16 15:30:00 +0300

    Fix Barriers PK overlays: sizing, drum passport selection, no duplicate sections (pkb-v140)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pkb-v139';
+const CACHE = 'pkb-v140';
 const PRECACHE = ['/', '/manifest.json', '/apple-touch-icon.png', '/favicon.png', '/icon-192.png', '/icon-512.png', '/badge.svg'];
 
 self.addEventListener('install', e => {
