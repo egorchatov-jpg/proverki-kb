c86116c9233aa8e88af9141e0a6bb66f131458e1
egorchatov-jpg <egorchatov@gmail.com>
2026-07-16 13:42:29 +0300

    Password settings: icon buttons and revert to last saved (pkb-v135)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pkb-v134';
+const CACHE = 'pkb-v135';
 const PRECACHE = ['/', '/manifest.json', '/apple-touch-icon.png', '/favicon.png', '/icon-192.png', '/icon-512.png', '/badge.svg'];
 
 self.addEventListener('install', e => {
