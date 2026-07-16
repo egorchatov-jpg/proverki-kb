ac332d62544a3b2af4d3b2d14c12f68d62480cc6
egorchatov-jpg <egorchatov@gmail.com>
2026-07-16 14:02:04 +0300

    Reset SOKB and corrective list filters on Back (pkb-v137)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pkb-v136';
+const CACHE = 'pkb-v137';
 const PRECACHE = ['/', '/manifest.json', '/apple-touch-icon.png', '/favicon.png', '/icon-192.png', '/icon-512.png', '/badge.svg'];
 
 self.addEventListener('install', e => {
