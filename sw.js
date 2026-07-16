416daeb89805e5a52757abd551d9338fe3320fb4
egorchatov-jpg <egorchatov@gmail.com>
2026-07-16 15:59:25 +0300

    Fix iOS keyboard for BPK passport/barrier name inputs (pkb-v143)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pkb-v142';
+const CACHE = 'pkb-v143';
 const PRECACHE = ['/', '/manifest.json', '/apple-touch-icon.png', '/favicon.png', '/icon-192.png', '/icon-512.png', '/badge.svg'];
 
 self.addEventListener('install', e => {
