49380d62c4339d6df9b9212d4bd7a1da2c37ecee
egorchatov-jpg <egorchatov@gmail.com>
2026-07-16 15:48:48 +0300

    BPK barrier overlay: passport label 14px with word wrap (pkb-v142)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pkb-v141';
+const CACHE = 'pkb-v142';
 const PRECACHE = ['/', '/manifest.json', '/apple-touch-icon.png', '/favicon.png', '/icon-192.png', '/icon-512.png', '/badge.svg'];
 
 self.addEventListener('install', e => {
