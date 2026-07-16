48dacacc47bfa2d3ce0aa6bb92b06b1d6b82c98f
egorchatov-jpg <egorchatov@gmail.com>
2026-07-16 13:25:26 +0300

    Revert filter header counters to unique column values (pkb-v134)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pkb-v133';
+const CACHE = 'pkb-v134';
 const PRECACHE = ['/', '/manifest.json', '/apple-touch-icon.png', '/favicon.png', '/icon-192.png', '/icon-512.png', '/badge.svg'];
 
 self.addEventListener('install', e => {
