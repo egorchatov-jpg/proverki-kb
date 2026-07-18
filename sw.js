3a847578d6d0b1496c49cbdde0d1558899c8d323
egorchatov-jpg <egorchatov@gmail.com>
2026-07-18 14:49:39 +0300

    fix: show only per-barrier checklist criteria on passport open (pkb-v173)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'pkb-v172';
-const PRECACHE = ['/', '/manifest.json', '/apple-touch-icon.png', '/favicon.png', '/icon-192.png', '/icon-512.png', '/badge.svg', '/passports/gaz-01.json'];
+const CACHE = 'pkb-v173';
+const PRECACHE = ['/', '/manifest.json', '/apple-touch-icon.png', '/favicon.png', '/icon-192.png', '/icon-512.png', '/badge.svg', '/passports/gaz-01.json?v=v2'];
 
 self.addEventListener('install', e => {
   self.skipWaiting();
