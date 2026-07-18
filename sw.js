aae361fb833a3db6b24bae6488941f068dc1db77
egorchatov-jpg <egorchatov@gmail.com>
2026-07-18 14:53:47 +0300

    fix: passport barrier button click opens checklist screen (pkb-v174)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pkb-v173';
+const CACHE = 'pkb-v174';
 const PRECACHE = ['/', '/manifest.json', '/apple-touch-icon.png', '/favicon.png', '/icon-192.png', '/icon-512.png', '/badge.svg', '/passports/gaz-01.json?v=v2'];
 
 self.addEventListener('install', e => {
