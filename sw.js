4b966858f0ad2ca24a8cc3044af0dbe6878f9dbc
egorchatov-jpg <egorchatov@gmail.com>
2026-07-24 22:35:21 +0300

    Fix detail value cell width: shrink detail-val by 5% not row (pkb-v267)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const STATIC_CACHE = 'pkb-static-v266';
-const API_CACHE = 'pkb-api-v266';
+const STATIC_CACHE = 'pkb-static-v267';
+const API_CACHE = 'pkb-api-v267';
 
 const SHELL_PRECACHE = [
   '/',
