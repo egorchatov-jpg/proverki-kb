b8768e174e9f9072963fccb8ee2dcacdcbea5507
egorchatov-jpg <egorchatov@gmail.com>
2026-07-28 09:14:51 +0300

    Restrict checklist edit from detail to admin/superuser (pkb-v320)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const STATIC_CACHE = 'pkb-static-v319';
-const API_CACHE = 'pkb-api-v319';
+const STATIC_CACHE = 'pkb-static-v320';
+const API_CACHE = 'pkb-api-v320';
 
 const SHELL_PRECACHE = [
   '/',
