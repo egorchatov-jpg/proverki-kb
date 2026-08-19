8223eeebc4735b47229d5277ac84057d79abf689
egorchatov-jpg <egorchatov@gmail.com>
2026-08-19 17:44:48 +0300

    pkb-v347: uniform button heights/widths in photo screen and back buttons
    - photo-action-btn height set to 53px (same as back button)
    - screen-find / screen-meropr-menu / screen-card-photo back buttons
      now use same padding-based width as detail-back-btn (40px horiz)
    - APP_BUILD and SW cache bumped to pkb-v347

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const STATIC_CACHE = 'pkb-static-v346';
-const API_CACHE = 'pkb-api-v346';
+const STATIC_CACHE = 'pkb-static-v347';
+const API_CACHE = 'pkb-api-v347';
 
 const SHELL_PRECACHE = [
   '/',
