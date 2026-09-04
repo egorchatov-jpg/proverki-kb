aea5b86e443a95495a033e158ad9e2858e341cab
egorchatov-jpg <egorchatov@gmail.com>
2026-09-04 12:39:27 +0300

    pkb-v472: zapas window - further width reduction (3%), zapas button uses natural text wrapping

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const STATIC_CACHE = 'pkb-static-v471';
-const API_CACHE = 'pkb-api-v471';
+const STATIC_CACHE = 'pkb-static-v472';
+const API_CACHE = 'pkb-api-v472';
 
 const SHELL_PRECACHE = [
   '/',
