21991056bbaa188a76908b2a9417168f0a81425b
egorchatov-jpg <egorchatov@gmail.com>
2026-09-04 21:01:29 +0300

    pkb-v476: fix org-working card layout - pill spans all columns so org data starts correctly

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const STATIC_CACHE = 'pkb-static-v475';
-const API_CACHE = 'pkb-api-v475';
+const STATIC_CACHE = 'pkb-static-v476';
+const API_CACHE = 'pkb-api-v476';
 
 const SHELL_PRECACHE = [
   '/',
